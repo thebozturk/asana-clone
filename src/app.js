ecd5f0c551ae69dbea8ef22e1d9390e46e6662a3
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2021-10-28 00:19:11 +0300

    initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const helmet = require("helmet");
+const config = require("./config");
+
+config();
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Listening on ${process.env.APP_PORT}`);
+});
