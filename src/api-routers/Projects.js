2516a81ae4d53caefd694f1a51c5b362af2fc019
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2021-11-29 23:45:37 +0300

    add new routers

--- src/api-routers/Projects.js
+++ src/api-routers/Projects.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { create, index } = require("../controllers/Projects");
+const { create, index, update } = require("../controllers/Projects");
 const schemas = require("../validations/Projects");
 const validate = require("../middlewares/validate");
 const authenticateToken = require("../middlewares/authenticate");
@@ -10,5 +10,8 @@ router.route("/").get(authenticateToken, index);
 router
   .route("/")
   .post(validate(schemas.createValidation), authenticateToken, create);
+router
+  .route("/:id")
+  .patch(authenticateToken, validate(schemas.updateValidation), update);
 
 module.exports = router;
--- src/api-routers/Users.js
+++ src/api-routers/Users.js
@@ -1,6 +1,14 @@
 const express = require("express");
-const { create, index, login } = require("../controllers/Users");
+const {
+  create,
+  index,
+  login,
+  projectList,
+  resetPassword,
+  update,
+} = require("../controllers/Users");
 const schemas = require("../validations/Users");
+const authenticate = require("../middlewares/authenticate");
 const validate = require("../middlewares/validate");
 
 const router = express.Router();
@@ -8,5 +16,12 @@ const router = express.Router();
 router.get("/", index);
 router.route("/").post(validate(schemas.createValidation), create);
 router.route("/login").post(validate(schemas.loginValidation), login);
+router.route("/projects").get(authenticate, projectList);
+router
+  .route("/")
+  .patch(authenticate, validate(schemas.updateValidation), update);
+router
+  .route("/reset-password")
+  .post(validate(schemas.resetPasswordValidation), resetPassword);
 
 module.exports = router;
