07b2d86af9d8a52bd43da5bf1788ac8b286d5a9f
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2021-11-29 23:48:34 +0300

    create project index and update project api

--- src/controllers/Projects.js
+++ src/controllers/Projects.js
@@ -1,15 +1,5 @@
-const { insert, list } = require("../services/Projects");
+const { insert, list, modify } = require("../services/Projects");
 const httpStatus = require("http-status");
-
-const create = (req, res) => {
-  insert(req.body)
-    .then((response) => {
-      res.status(httpStatus.CREATED).send(response);
-    })
-    .catch((e) => {
-      res.status(httpStatus.INTERNAL_SERVER_ERROR).send(e);
-    });
-};
 
 const index = (req, res) => {
   list()
@@ -19,7 +9,37 @@ const index = (req, res) => {
     .catch((e) => res.status(httpStatus.INTERNAL_SERVER_ERROR).send(e));
 };
 
+const create = (req, res) => {
+  req.body.user_id = req.user;
+  insert(req.body)
+    .then((response) => {
+      res.status(httpStatus.CREATED).send(response);
+    })
+    .catch((e) => {
+      res.status(httpStatus.INTERNAL_SERVER_ERROR).send(e);
+    });
+};
+
+const update = (req, res) => {
+  console.log(req.params.id);
+  if (!req.params?.id) {
+    return res.status(httpStatus.BAD_REQUEST).send({
+      message: "Id information is missing",
+    });
+  }
+  modify(req.body, req.params.id)
+    .then((updatedProject) => {
+      res.status(httpStatus.OK).send(updatedProject);
+    })
+    .catch((e) =>
+      res
+        .status(httpStatus.INTERNAL_SERVER_ERROR)
+        .send({ error: "There was a problem during updating" })
+    );
+};
+
 module.exports = {
   create,
   index,
+  update,
 };
