ad5acd43af352a46318a8b88f40d289325a4fe2c
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2021-12-20 23:18:58 +0300

    Add the change password validation

--- src/validations/Users.js
+++ src/validations/Users.js
@@ -20,9 +20,14 @@ const resetPasswordValidation = Joi.object({
   email: Joi.string().email().required().min(8),
 });
 
+const changePasswordValidation = Joi.object({
+  password: Joi.string().required().min(8),
+});
+
 module.exports = {
   createValidation,
   loginValidation,
   resetPasswordValidation,
   updateValidation,
+  changePasswordValidation,
 };
