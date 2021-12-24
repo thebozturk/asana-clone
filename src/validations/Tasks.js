793bd52f2bc94c682363445edd94bb938698c7c5
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2021-12-24 16:40:24 +0300

    Update the tasks validation

--- src/validations/Tasks.js
+++ src/validations/Tasks.js
@@ -4,9 +4,8 @@ const createValidation = Joi.object({
   title: Joi.string().required().min(3),
   section_id: Joi.string().required().min(5),
   project_id: Joi.string().required().min(5),
-  description: Joi.string().required().min(3),
-  statuses: Joi.array().required(),
-  project_id: Joi.string().required().min(5),
+  description: Joi.string().min(3),
+  statuses: Joi.array(),
   order: Joi.number(),
   isCompleted: Joi.boolean(),
   comments: Joi.array(),
@@ -28,7 +27,14 @@ const updateValidation = Joi.object({
   assigned_to: Joi.string().min(5),
 });
 
+const commentValidation = Joi.object({
+  comment: Joi.string().min(3),
+  _id: Joi.string().min(5),
+  _destroy: Joi.boolean(),
+});
+
 module.exports = {
   createValidation,
   updateValidation,
+  commentValidation,
 };
