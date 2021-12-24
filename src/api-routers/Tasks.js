1f944f750bb501e4b24f9e2880f006efcfad3e9f
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2021-12-24 16:42:13 +0300

    Create the tasks service and controller then create the endpoints

--- src/api-routers/Tasks.js
+++ src/api-routers/Tasks.js
@@ -1,5 +1,13 @@
 const express = require("express");
-const { create, update, DeleteTask } = require("../controllers/Tasks");
+const {
+  create,
+  update,
+  DeleteTask,
+  makeComment,
+  deleteComment,
+  addSubTask,
+  fetchTask,
+} = require("../controllers/Tasks");
 const schemas = require("../validations/Tasks");
 const validate = require("../middlewares/validate");
 const authenticateToken = require("../middlewares/authenticate");
@@ -13,5 +21,13 @@ router
   .route("/:id")
   .patch(authenticateToken, validate(schemas.updateValidation), update);
 router.route("/:id").delete(authenticateToken, DeleteTask);
+router
+  .route("/:id/make-comment")
+  .post(authenticateToken, validate(schemas.commentValidation), makeComment);
+router.route("/:id/:commentId").delete(authenticateToken, deleteComment);
+router
+  .route("/:id/add-sub-task")
+  .post(validate(schemas.createValidation), authenticateToken, addSubTask);
+router.route("/:id").get(authenticateToken, fetchTask);
 
 module.exports = router;
